d8a1bea51cac70d676840197852904c940b929ec
sultani- <sultanishagufta058@gmail.com>
2026-07-18 11:05:06 -0700

    Add files via upload

---
+++ Drag and Drop File/main.js
@@ -0,0 +1,80 @@
+let progressBar = document.querySelector("#progress-bar");
+let fileInput = document.querySelector("#fileInput");
+let Btn = document.querySelector("#browseBtn");
+let dropArea = document.querySelector(".drop-area");
+
+function startProgress(callback){
+
+    let progress = 0;
+    progressBar.style.width = "0%";
+    progressBar.style.padding = "2rem";
+
+    const interval = setInterval(() =>{
+        progress += 10;
+    progressBar.style.width = progress + "%";
+
+    if(progress >= 100 ){
+        clearInterval(interval);
+        callback();
+    }
+    }, 300);
+ }
+Btn.addEventListener("click", () =>{
+    fileInput.click();
+});
+
+fileInput.addEventListener("change", function(){
+    const file = fileInput.files[0];
+
+    if (!file) return;
+
+    const  allowedTypes = [
+        "image/jpg",
+        "image/png",
+        "image/gif"
+    ];
+
+    if(!allowedTypes.includes(file.type)){
+        alert("JPG, PNG, GIF");
+        return;
+    }
+    startProgress(() =>{
+        console.log("Progress Complete");
+        
+    })
+
+    const reader = new FileReader();
+
+        reader.onload = function (e) {
+
+            startProgress(() =>{
+                
+                document.getElementById("preview").innerHTML= `<img style="width:200px" src="${e.target.result}" alt="Preview">`;
+               const img = document.querySelector("img");
+               img.style.width = "300px";
+                 img.style.marginTop = "2rem";
+                img.style.borderRadius = "1rem";
+
+                localStorage.setItem("UploadImage", e.target.result);
+            })
+        };
+        reader.readAsDataURL(file);
+    });
+
+    const savedImage = localStorage.getItem("UploadImage");
+    if(savedImage){
+       document.getElementById("preview").innerHTML = `<img src="${savedImage}"
+       style="width:300px; margin-top:2rem; border-radius: 1rem ;">`;
+    }
+
+fileInput.addEventListener("change", function(){
+    const file = fileInput.f
+})
+
+dropArea.addEventListener("dragover", (e) =>{
+    e.preventDefault();
+
+    const file = e.dataTransfer.files[0];
+    console.log(file);
+});
+
